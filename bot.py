cd457e0523a429429fc061380a9cce13d3c8635d
DeveloperJaat <97823189+DeveloperJaat@users.noreply.github.com>
2025-03-29 15:42:35 +0530

    1

--- bot.py
+++ bot.py
@@ -35,8 +35,6 @@ from Naman.bot.clients import initialize_clients
 ppath = "plugins/*.py"
 files = glob.glob(ppath)
 NamanBot.start()
-loop = asyncio.get_event_loop()
-
 
 async def start():
     print('\n')
@@ -92,9 +90,8 @@ async def start():
     await idle()
 
 
-if __name__ == '__main__':
+if name == 'main':
     try:
-        loop.run_until_complete(start())
+        asyncio.run(start())
     except KeyboardInterrupt:
         logging.info('Service Stopped Bye 👋')
-
