90cd6b39f2729152e3e140a1343e6fb8fdf57898
MrTG-CodeBot <148360334+MrTG-CodeBot@users.noreply.github.com>
2025-03-29 18:59:44 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -35,8 +35,6 @@ from Naman.bot.clients import initialize_clients
 ppath = "plugins/*.py"
 files = glob.glob(ppath)
 NamanBot.start()
-loop = asyncio.get_event_loop()
-
 
 async def start():
     print('\n')
@@ -94,7 +92,7 @@ async def start():
 
 if __name__ == '__main__':
     try:
-        loop.run_until_complete(start())
+        asyncio.run(start())
     except KeyboardInterrupt:
         logging.info('Service Stopped Bye 👋')
 
